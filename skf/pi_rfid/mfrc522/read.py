ad125d72403a1ec5d43840bf7672b8db073ad058
Sverre Furseth <sverre.furseth@fremtind.no>
2024-02-09 14:42:48 +0100

    test

--- skf/pi_rfid/mfrc522/read.py
+++ skf/pi_rfid/mfrc522/read.py
@@ -3,7 +3,8 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-try:
+try: 
+        print("Nå skal vi se om vi får data ")
         id, text = reader.read()
         print(id)
         print(text)
